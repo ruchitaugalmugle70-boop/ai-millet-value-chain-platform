bf0684822c9555c18830b805545dcd7dcce19b74
Ruchita <admin@Ruchita-MacBook-Air.local>
2026-02-27 21:33:07 +0530

    config file

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+class Config:
+    MONGO_URI = "mongodb://localhost:27017/"
+    DB_NAME = "farmer_advisory"
+    JWT_SECRET_KEY = "super-secret-key"
+    WEATHER_API_KEY = "09382f3811b878954f982e175b72ee69"
+    MANDI_API_KEY = "579b464db66ec23bdd000001a984b9393cf748925f0619debd11f478"
